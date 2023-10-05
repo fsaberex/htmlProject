3fecdc9bdf2db548b0ad9b7da6159a578604ee1f
JeffBenton <jeffbjr56@gmail.com>
2023-10-05 16:11:58 -0400

    Re-worked Javascript to check for correct Zipcode

--- aboutMe/onlineOrder.js
+++ aboutMe/onlineOrder.js
@@ -4,20 +4,21 @@ let show = false;
 
 let button = document.getElementById('check');
 
-let count = 0;
+//let count = 0;
+
+const validZip = ["20170", "20171", "20172", "20190", "20191", "20192", "20194", "20195", "20196", "20598", "22095", "22096"];
 
 button.addEventListener('click', () => {
     
     show = !show;
 
-    let result = document.getElementById('orderResult');
+    let result = document.getElementById('zip').value;
+    console.log(result);
 
-    if (show) {
+    if (console.log(validZip.includes(result))) {
         result.style.display = 'block';
     }
     else {
         result.style.display = 'none';
     }
 })
-
-const validZip = ["20170", "20171", "20172", "20190", "20191", "20192", "20194", "20195", "20196", "20598", "22095", "22096"];
