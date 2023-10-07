031adfd1c8e5cc8edc960eb02ab0ff805ee63701
JeffBenton <jeffbjr56@gmail.com>
2023-10-06 20:02:04 -0400

    final presentation touches

--- aboutMe/onlineOrder.js
+++ aboutMe/onlineOrder.js
@@ -4,21 +4,22 @@ let show = false;
 
 let button = document.getElementById('check');
 
-//let count = 0;
-
 const validZip = ["20170", "20171", "20172", "20190", "20191", "20192", "20194", "20195", "20196", "20598", "22095", "22096"];
 
 button.addEventListener('click', () => {
     
     show = !show;
 
+    let zipResult = document.getElementById('zipResult');
+    let sadPizza = document.getElementById('sadPizza');
+
     let result = document.getElementById('zip').value;
     console.log(result);
 
     if (console.log(validZip.includes(result))) {
-        result.style.display = 'block';
+        zipResult.style.display = 'block';
     }
     else {
-        result.style.display = 'none';
+        sadPizza.style.display = 'block';
     }
 })
